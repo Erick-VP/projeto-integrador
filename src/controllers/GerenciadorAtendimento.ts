08ebd415c4bad25d367951ac7e54f7c8035ee23a
Erick-VP <erickvscletal31@gmail.com>
2025-05-22 07:34:52 -0300

    adicionei os tempos da prioridade, falta verificar

--- src/controllers/GerenciadorAtendimento.ts
+++ src/controllers/GerenciadorAtendimento.ts
@@ -1,12 +1,24 @@
 import { Triagem } from "../entities/triage";
+import { Prioridade } from "../priorities/priority";
 
 export class GerenciadorAtendimento {
     filaComTempo: Triagem[] = [];
     filaSemTempo: Triagem[] = [];
     limiteMinimo: number = 30; // em minutos
-
+    limites: Map<Prioridade, number> = new Map([
+        [Prioridade.Vermelho, 0],
+        [Prioridade.Laranja, 10],
+        [Prioridade.Amarelo, 60],
+        [Prioridade.Verde, 120],
+        [Prioridade.Azul, 240],
+    ]);
+    
     public adicionarTriagem(t: Triagem): void {
+        if (t.prioridade === Prioridade.Vermelho) {
+            this.filaSemTempo.push(t);
+        }else {
         this.filaComTempo.push(t);
+        }
     }
 
     public verificarETrocarFila(): void {
