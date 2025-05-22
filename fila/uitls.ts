d0d7152ebdbaefa830fad83811171fc1c3226e81
Paulo Henrique Alves Vieira <alvespaulo593@gmail.com>
2025-05-22 15:29:51 -0300

    adicionei as filas que serão incrementadas

---
+++ fila/uitls.ts
@@ -0,0 +1,36 @@
+const vermelho = {
+    cor: 'vermelho',
+    prioridade: 5,
+    tempo: 0
+}
+const laranja = {
+    cor: 'laranja',
+    prioridade: 4,
+    tempo: 1*60*1000
+    //3
+}
+const amarelo = {
+    cor: 'amarelo',
+    prioridade: 3,
+    tempo: 2*60*1000
+    //53
+}
+const verde = {
+    cor: 'verde',
+    prioridade: 2,
+    tempo: 3*60*1000
+    //113
+}
+const azul = {
+    cor: 'azul',
+    prioridade: 1,
+    tempo: 4*60*1000
+    //233
+}
+export const prioridades = {
+    vermelho,
+    laranja,
+    amarelo,
+    verde,
+    azul
+}
