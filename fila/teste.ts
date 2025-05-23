108f7cb15b2bc321f1dd4e27a15fe9d7ce5ad7b8
Erick-VP <erickvscletal31@gmail.com>
2025-05-23 07:41:27 -0300

    tirei pasta filas do paulo

--- fila/teste.ts
+++
@@ -1,8 +0,0 @@
-const uuuuu: number[] = [];
-console.log(uuuuu.fill(50,1,50))
-const n :number = 5
-const array1: number[] = new Array(n).fill(0)
-console.log(array1)
-
-const uuuuu: number[] = [];
-console.log(uuuuu.fill(50))
